d01b291b909fb08c92357e08c8317f1b83848101
Fauzan4603 <fauzannasihin5@upi.edu>
2023-10-12 16:48:39 +0700

    js

---
+++ main.js
@@ -0,0 +1,39 @@
+//console.log("Halo bro")
+//alert("ada notifikasi")
+//prompt("pinjem seratus")
+
+//var Promnet = "Coding is easy"
+//console.log(Promnet)
+//var Promnet = "Coding is not easy"
+//console.log(Promnet)
+//var Promnet = "nothing is possible"
+//console.log(Promnet)
+
+// let Promnet="Coding is fun"
+// console.log(Promnet)
+// let Promnet="Coding is not fun"
+// console.log(Promnet)
+
+// const Promnet="coding is my life"
+// console.log(Promnet);
+// const Promnet="coding is not my life"
+// console.log(Promnet);
+
+// let totalPoint = prompt ("Masukkan point anda")
+// if (totalPoint >= 100){
+//     document.write("congratulation")
+// }
+// else{
+//     document.write("keep trying")
+// }
+
+let [x, y] = [6, 3]
+console.log(x<10 && y>1)
+console.log(x<10 && y<1)
+console.log(x==5||y==5)
+console.log(x==6||y==5)
+console.log(!(x==y))
+
+document.write(x<10 && y>1)
+document.write("<br>")
+document.write(x<10 && y<1)
