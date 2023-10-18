850d71003dca1ffa36eb9a5db1d45327efe8cdf0
Fauzan4603 <fauzannasihin5@upi.edu>
2023-10-18 19:01:13 +0700

    Update Checkbox

--- main.js
+++ main.js
@@ -38,11 +38,52 @@
 // document.write("<br>")
 // document.write(x<10 && y<1)
 
-let p = document.querySelector("p")
-let button = document.querySelector("button")
-let input = document.querySelector("input")
-button.addEventListener('click', function(){
-    let isi = input.value
-    console.log(isi)
-    p.innerHTML = isi
-})
+// let p = document.querySelector("p")
+// let button = document.querySelector("button")
+// let input = document.querySelector("input")
+// button.addEventListener('click', function(){
+//     let isi = input.value
+//     console.log(isi)
+//     p.innerHTML = isi
+// })
+function addTask() {
+    const taskText = document.getElementById("task").value;
+    if (taskText === "") {
+        alert("Harap masukkan tugas!");
+        return;
+    }
+
+    const taskList = document.getElementById("taskList");
+    const taskItem = document.createElement("li");
+    const span = document.createElement("span");
+    const id = "" + (new Date()).getTime();
+
+    const check = document.createElement("input");
+    check.type = "checkbox";
+    check.id = id;
+    check.addEventListener("change", function() {
+        check.nextElementSibling.classList.toggle("checked");
+    });
+
+    const label = document.createElement("label");
+    label.htmlFor = id;
+    label.innerText = taskText;
+
+    span.appendChild(check);
+    span.appendChild(label);
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete-button";
+    deleteButton.addEventListener("click", function deleteTask(e) {
+        const taskList = document.getElementById("taskList");
+        const taskItem = e.target.parentElement;
+        taskList.removeChild(taskItem);
+    });
+    deleteButton.innerText = "X";
+    
+    taskItem.appendChild(span);
+    taskItem.appendChild(deleteButton);
+    taskList.appendChild(taskItem);
+
+    document.getElementById("task").value = "";
+}
